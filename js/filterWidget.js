852bc9bee55af15004cf7064a621c1cd28d788b2
Krzysiek <hakkai660@gmail.com>
2017-06-11 09:24:23 +0200

    'fix/refactor'

--- js/filterWidget.js
+++ js/filterWidget.js
@@ -2,55 +2,54 @@
  * Created by Hp on 2017-03-06.
  */
 
-// const = new Array['all', 'red', 'blue', 'green', 'yellow', 'orange'];
+const categories = ['all', 'red', 'blue', 'green', 'yellow', 'orange'];
 
-//# TODO change to ES6
-var categories = ['all', 'red', 'blue', 'green', 'yellow', 'orange'];
+const createCategoriesMenu = () => {
+    for (let i = 0; i < categories.length; i++) {
+        let menuClass = "cat-" + categories[i];
 
-for (var i = 0; i < categories.length; i++) {
-    var menuClass = "cat-" + categories[i];
-
-    document.getElementById("categoriesAll").innerHTML += "<li class=" + 'menu-' + menuClass + "><a href= " + '#' + menuClass + ">" + categories[i] + "</a></li>";
+        document.getElementById("categoriesAll").innerHTML += "<li class=" + 'menu-' + menuClass + "><a href= " + '#' + menuClass + ">" + categories[i] + "</a></li>";
+    }
 }
 
-for (var i = 0; i < 20; i++) {
-    var randNum = Math.floor((Math.random() * 5) + 1);
+const createTiles = () => {
+    for (let i = 0; i < 20; i++) {
+        let randNum = Math.floor((Math.random() * 5) + 1);
 
-    document.getElementById("tiles").innerHTML += "<li class=" + 'cat-' + categories[randNum] + ">Tile -" + [i] + "</li>";
-
+        document.getElementById("tiles").innerHTML += "<li class=" + 'cat-' + categories[randNum] + ">Tile -" + [i] + "</li>";
+    }
 }
 
-$(document).ready(function () {
+createCategoriesMenu();
+createTiles();
 
-    $(function () {
-        $('.categories li a').click(function () {
-            $('.categories li a').removeClass('active');
-            $(this).addClass('active');
-            var href = $(this).attr("href");
-            // console.log(href);
+$(function () {
+    $('.categories li a').click(function () {
+        $('.categories li a').removeClass('active');
+        $(this).addClass('active');
+        let href = $(this).attr("href");
+        // console.log(href);
 
-            var color = href.replace("#", ".");
+        let color = href.replace("#", ".");
 
-            // console.log(color);
+        // console.log(color);
 
-            $('.tiles li').not(color).each(function () {
-                $(this).addClass("hide");
-                $(this).removeClass("show");
-            })
+        $('.tiles li').not(color).each(function () {
+            $(this).addClass("hide");
+            $(this).removeClass("show");
+        })
 
-            $(color).each(function () {
-                $(this).addClass("show");
+        $(color).each(function () {
+            $(this).addClass("show");
+            $(this).removeClass("hide");
+        })
+
+        if (color == ".cat-all") {
+            $('.tiles li').each(function () {
                 $(this).removeClass("hide");
             })
+        } // end if statment
 
-            if (color == ".cat-all") {
-                $('.tiles li').each(function () {
-                    $(this).removeClass("hide");
-                })
-            } // end if statment
+    }); // end click function
 
-        }); // end click function
-
-    }); // end function
-
-}); // end document ready
+}); // end function
--- js/learnEnglishWidget.js
+++ js/learnEnglishWidget.js
@@ -2,81 +2,6 @@
  * Created by Hp on 2017-02-25.
  */
 
-//# TODO Change all to jQuery
-//var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-
-// $(document).ready(function() {
-
-// download json
-// $("#dictionary-btn").click(function() {
-//     $.getJSON("../js/data.json", function(data) {
-//         var products = data.words;
-//         var ul = $("<ul>");
-//         var items = '';
-//         $.each(products, function(i, v) {
-//             items += "<li>" + v.english + "</li>";
-//             items += "<li>" + v.polish + "</li>";
-//         });
-//         ul.html(items);
-//         $('#content').append(ul);
-//         $("#dictionary-btn").off('click');
-//     });
-// });
-
-//# TODO Add MongoDB (sending and taking from DB)
-
-// add to JSON
-// $('#btnSave').click(function () {
-//     var english = $("#english").val();
-//     var polish = $("#polish").val();
-//
-//     $.getJSON("../js/data.json", function(data) {
-//         // console.log(data.words[1]);
-//         var data1 = {
-//             english: "test",
-//             polish: "test"
-//         };
-//         data.words.push(data1);
-//     });
-//
-// });
-
-//     $("#btn_random").on("click", getQuote);
-//
-//     $("#btn_random").click(function() {
-//
-//         $.getJSON("../js/data.json", function(data) {
-//
-//             var words = data.words;
-//
-//             var num = Math.floor(Math.random() * words.length);
-//
-//             $('#english_word').html(words[num].english);
-//             $('#polish_word').html(words[num].polish);
-//
-//         });
-//
-//     });
-//
-// }); // end jQuery
-
-
-//
-// //# TODO Remove not use code
-// function getQuote() {
-//     var color = Math.floor(Math.random() * colors.length);
-//
-//     for (var i = 0; i < colors.length; i++) {
-//         var randCol = colors[color];
-//     }
-//
-//     $("element zmieniajacy kolor").css({
-//         backgroundColor: randCol
-//     }).animate(1000);
-//
-// }
-
-
 (function () {
     'use strict';
 
@@ -161,7 +86,7 @@
     }
 
     const createWordListItem = (todo) => {
-        console.log(todo);
+        randomWord(todo);
         let checkbox = document.createElement('input');
         checkbox.id = todo._id;
         checkbox.type = 'checkbox';
@@ -191,6 +116,23 @@
         }
 
         return li;
+    }
+
+    const randomWord = (todo) => {
+        console.log(todo);
+        // console.log(todo._id[0]);
+        const btnRand = document.getElementById('btn_random');
+        btnRand.onclick = () => {
+            //let index =  Math.floor(Math.random() * 100).toString();
+            let index =  "95";
+            // let index = "19";
+            console.log(index);
+            if (index === todo._id) {
+                console.log(this.todo.title);
+            }
+            //console.log(todo._id[index]);
+        }
+
     }
 
     addEventListeners();
